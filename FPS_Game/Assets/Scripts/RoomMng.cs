871658fd817f57199bf34689fa5110b7cfe74019
DESKTOP-VU6LCTF\khanh <khanhbklc12@gmail.com>
2018-03-25 02:46:18 +0900

    apply multiplayer connect

---
+++ FPS_Game/Assets/Scripts/RoomMng.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMng : Photon.MonoBehaviour {
+
+    public string verNum = "0.0.1";
+    public string roomName = "TestRoom";
+    public Transform spawnPts;
+    public GameObject playerPref;
+    public bool isConnected = false;
+    public RoomOptions roomOps;
+    public TypedLobby typeLobby;
+
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(verNum);
+        Debug.Log("Start connecting...");
+    }
+
+    public void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomName, null, null);
+
+    }
+
+    public void OnJoinedRoom()
+    {
+        isConnected = true;
+        SpawnPlayer();
+        Debug.Log("Connected !");
+    }
+
+    public void SpawnPlayer()
+    {
+        GameObject player = PhotonNetwork.Instantiate(playerPref.name, spawnPts.position, spawnPts.rotation, 0) as GameObject;
+        CharacterController controller = player.GetComponent<CharacterController>();
+        Camera cam = player.GetComponentInChildren<Camera>();
+        controller.enabled = true;
+        cam.enabled = true;
+    }
+}
