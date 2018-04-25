51129fe607ee05a4c5495ccd95d44c33e5b5cd8e
CLJOKER\QuyenLinh <khanhbklc12@gmail..com>
2018-04-26 02:29:16 +0900

    add shoot animation, fix shoot in multi client, add fire rate, crosshair

--- FPS_Game/Assets/Scripts/wepScript.cs
+++ FPS_Game/Assets/Scripts/wepScript.cs
@@ -8,17 +8,48 @@ public class wepScript : MonoBehaviour {
     public GameObject hitPar;
     public int damage = 30;
     public int range = 10000;
+    [SerializeField] private Animator anim;
+    private PhotonView photonView;
+
+    public float fireRate = 0.1f;
+    float fireTimer;
+
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
 
     private void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(photonView.isMine)
         {
-            FireShot();
+            if (Input.GetMouseButton(0))
+            {
+                FireShot();
+
+            }
+
+            if (fireTimer < fireRate)
+            {
+                fireTimer += Time.deltaTime;
+            }
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+        if(info.IsName("Shoot"))
+        {
+            anim.SetBool("Firing", false);
         }
     }
 
     public void FireShot()
     {
+        if (fireTimer < fireRate) return;
+
         RaycastHit hit;
 
         Ray ray = fpsCam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
@@ -27,13 +58,18 @@ public class wepScript : MonoBehaviour {
         {
 
 
-            if(hit.transform.tag == "Player")
+            if(hit.transform.tag == "Soldier_Player")
             {
                 hit.transform.GetComponent<PhotonView>().RPC("applyDamage", PhotonTargets.All, damage);
-                Debug.Log("hit!");
             }
             GameObject par;
+
+            
             par = PhotonNetwork.Instantiate(hitPar.name, hit.point, Quaternion.LookRotation(hit.normal), 0) as GameObject;
         }
+
+        anim.SetBool("Firing", true);
+
+        fireTimer = 0.0f;
     }
 }
